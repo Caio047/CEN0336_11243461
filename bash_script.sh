2d0b23d08ac63fef719699c734b105b417b27421
Caio047 <caio_mendes@usp.br>
2022-10-15 11:55:35 -0300

    Revert "rm"
    This reverts commit 920d26a9f8965f0744a224b4b467f54144f75999.

---
+++ bash_script.sh
@@ -0,0 +1,3 @@
+pwd # Prints the current workind directory
+ls -la /home > list_HOME.txt # Generates a .txt file containing the detailed contents of the home directory
+date  --rfc-3339='date' # Prints the current date
